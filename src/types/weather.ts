b1c08bbe13fff4e7b878776138b630f935121409
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-10-23 10:43:02 +0800

    - feat: define type

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,7 +1,44 @@
-import { City } from './city';
-import { WeatherList } from './weatherList';
+export interface WeatherData {
+  type: 'data';
+  city: string;
+  weatherList: WeatherDetail[];
+}
 
-export interface Weather {
-  list: WeatherList[];
+export interface WeatherError {
+  type: 'error';
+  error: string;
+}
+
+export interface APIError {
+  code: string;
+  message: string;
+}
+
+export interface WeatherDetail {
+  dateTime: string;
+  humidity: number;
+  temp: number;
+  maxTemp: number;
+  minTemp: number;
+}
+
+export interface City {
+  name: string;
+}
+
+export interface RawWeather {
+  humidity: number;
+  temp: number;
+  temp_max: number;
+  temp_min: number;
+}
+
+export interface RawWeatherDetail {
+  dt_txt: string;
+  main: RawWeather;
+}
+
+export interface RawWeatherData {
   city: City;
+  list: RawWeatherDetail[];
 }
