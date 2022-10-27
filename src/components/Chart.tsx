f74a201d96d94d2ce96092d4d7dc6a583df3e7cb
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-10-27 08:57:21 +0800

    - fix: PieChart not showing correctly - refactor: adjust the BarChart height formula - refactor: round temperature - chore: edit README

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,6 +1,3 @@
-import { createStructuredSelector } from 'reselect';
-import { useAppSelector } from '../hooks/hooks';
-import { RootState } from '../store/store';
 import { WeatherDetail } from '../types/weather';
 import BarChart from './BarChart';
 import PieChart from './PieChart';
@@ -9,32 +6,24 @@ interface Props {
   weatherList: WeatherDetail[];
 }
 
-const weatherSelector = createStructuredSelector({
-  unit: (state: RootState) => state.weather.unit,
-});
-
 function Chart({ weatherList }: Props) {
-  const { unit } = useAppSelector(weatherSelector);
-
   return (
     <div>
       <div className="flex flex-col items-center md:flex-row md:justify-between">
         <BarChart
-          title={`Max Temperature( ${unit === 'metric' ? '°C' : '°F'})`}
+          title="Max Temperature"
           list={weatherList.map((i) => i.maxTemp)}
           date={weatherList.map((i) => i.dateTime)}
         />
         <BarChart
-          title={`Max Temperature( ${unit === 'metric' ? '°C' : '°F'})`}
+          title="Min Temperature"
           list={weatherList.map((i) => i.minTemp)}
           date={weatherList.map((i) => i.dateTime)}
         />
       </div>
 
       <div className="mt-10">
-        <p className="text-center text-xl font-bold text-primary">
-          Humidity(%)
-        </p>
+        <p className="text-center text-xl font-bold text-primary">Humidity</p>
         <div className="flex md:justify-center mt-4 overflow-auto">
           {weatherList.map((i) => (
             <PieChart
