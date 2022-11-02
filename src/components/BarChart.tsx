96af36758445ce1dea2d4f980e8177c3e5b8e12c
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-11-02 10:23:49 +0800

    - refactor: percentage of BarChart

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -10,11 +10,11 @@ function BarChart({ title, list, date }: Props) {
 
   return (
     <div className="mt-10">
-      <p className="text-center text-xl font-bold text-primary">{title}</p>
+      <p className="text-center text-xl font-bold text-primary mb-14">{title}</p>
       <div className="flex mt-1">
         {list.map((item, index) => (
           <div key={index} className="table">
-            <div className="table-cell align-bottom h-36">
+            <div className="table-cell align-bottom h-28">
               <div className="text-center text-third text-sm font-bold">
                 {item.toFixed(1)}
               </div>
@@ -22,7 +22,7 @@ function BarChart({ title, list, date }: Props) {
               <div
                 className="mx-1 bg-secondary w-12 rounded-t-lg hover:bg-third animate-barChart"
                 style={{
-                  height: `${(60 * (item - min)) / (max - min) + 15}%`,
+                  height: `${(80 * (item - min)) / (max - min) + 20}%`,
                 }}
               />
               <p className="text-center text-third text-sm font-bold">
